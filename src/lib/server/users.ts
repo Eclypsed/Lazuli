2b79dcdd0a977a97aaef744a559ab5018ceccb8c
Eclypsed <nicholas.tamassia@gmail.com>
2024-01-23 12:05:33 -0500

    Redoing database architecture

--- src/lib/server/users.ts
+++ src/lib/server/users.ts
@@ -5,12 +5,33 @@ import { generateUUID } from '$lib/utils'
 const db = new Database('./src/lib/server/users.db', { verbose: console.info })
 db.pragma('foreign_keys = ON')
 const initUsersTable = 'CREATE TABLE IF NOT EXISTS Users(id VARCHAR(36) PRIMARY KEY, username VARCHAR(30) UNIQUE NOT NULL, password VARCHAR(72) NOT NULL)'
+const initConnectionsTable =
+    'CREATE TABLE IF NOT EXISTS Connections(id VARCHAR(36) PRIMARY KEY, userId VARCHAR(36) NOT NULL, serviceType VARCHAR(64) NOT NULL, serviceUser TEXT NOT NULL, serviceUrl TEXT NOT NULL, accessToken TEXT NOT NULL, refreshToken TEXT, expiry INTEGER, FOREIGN KEY(userId) REFERENCES Users(id))'
 db.exec(initUsersTable)
+db.exec(initConnectionsTable)
 
 export interface User {
     id: string
     username: string
     password: string
+}
+
+export interface Service {
+    url: string
+    serviceType: 'jellyfin' | 'youtube-music'
+    displayName: string
+    icon: string
+    primaryColor: string
+}
+
+export interface Connection {
+    id: string
+    user: User
+    service: Service
+    serviceUser: string
+    accessToken: string
+    refreshToken: string | null
+    expiry: number | null
 }
 
 export class Users {
@@ -23,4 +44,10 @@ export class Users {
     static getUser = (id: string): User => {
         return db.prepare('SELECT * FROM Users WHERE id = ?').get(id) as User
     }
+}
+
+export class Connections {
+    // static getConnection = (id: string): Connection => {
+    //     const connectionRow = db.prepare('SELECT * FROM Connections WHERE id = ?').get(id)
+    // }
 }

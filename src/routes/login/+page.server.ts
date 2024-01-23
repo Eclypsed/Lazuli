701fc69bf0c13def467b6dc6c8c1b3f65bff24b5
Eclypsed <nicholas.tamassia@gmail.com>
2024-01-23 01:22:49 -0500

    forgot a file

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,6 +2,8 @@ import { SECRET_JWT_KEY } from '$env/static/private'
 import { fail, redirect } from '@sveltejs/kit'
 import { genSaltSync, hashSync } from 'bcrypt-ts'
 import type { PageServerLoad, Actions } from './$types'
+import { Users } from '$lib/server/users'
+import type { User } from '$lib/server/users'
 
 export const load: PageServerLoad = async ({ url }) => {
     const redirectLocation = url.searchParams.get('redirect')
